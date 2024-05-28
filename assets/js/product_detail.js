3d50221e0edd62845fb00b65367bc00312a3215f
DangVanCong2301 <dangvancong2301@gmail.com>
2024-05-28 10:29:35 +0700

    edit responsive product detail page

--- assets/js/product_detail.js
+++ assets/js/product_detail.js
@@ -4,3 +4,34 @@ function loadImage() {
     }, 1000);
 }
 loadImage();
+
+window.addEventListener('scroll', () => {
+    const y = this.pageYOffset;
+    if (y > 80) {
+        this.document.querySelector(".header__product-detail").classList.add("scroll-detail");
+        this.document.querySelector(".header__mobile-back").classList.add("scroll-detail");
+        this.document.querySelector(".header__mobile-product-name").classList.add("scroll-detail");
+        this.document.querySelector(".header__cart-wrap").classList.add("scroll-detail");
+        this.document.querySelector(".header__mobile-more").classList.add("scroll-detail");
+        this.document.querySelector(".header__mobile-back-icon").classList.add("scroll-detail");
+        this.document.querySelector(".header__cart-icon").classList.add("scroll-detail");
+        this.document.querySelector(".header__mobile-more-icon").classList.add("scroll-detail");
+    } else {
+        this.document.querySelector(".header__product-detail").classList.remove("scroll-detail");
+        this.document.querySelector(".header__mobile-back").classList.remove("scroll-detail");
+        this.document.querySelector(".header__mobile-product-name").classList.remove("scroll-detail");
+        this.document.querySelector(".header__cart-wrap").classList.remove("scroll-detail");
+        this.document.querySelector(".header__mobile-more").classList.remove("scroll-detail");
+        this.document.querySelector(".header__mobile-back-icon").classList.remove("scroll-detail");
+        this.document.querySelector(".header__cart-icon").classList.remove("scroll-detail");
+        this.document.querySelector(".header__mobile-more-icon").classList.remove("scroll-detail");
+    }
+});
+
+function showBottomSheet() {
+    document.querySelector(".detail__mobile-bottom-sheet").classList.add("open");
+}
+
+function closeBottomSheet() {
+    document.querySelector(".detail__mobile-bottom-sheet").classList.remove("open");
+}
