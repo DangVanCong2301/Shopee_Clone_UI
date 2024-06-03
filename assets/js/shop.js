46826f9e733b32915465c2fca621b16cb28a06ee
DangVanCong2301 <dangvancong2301@gmail.com>
2024-06-03 09:53:59 +0700

    code shop category mobile

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,3 +1,47 @@
+const shopMobileTitle = document.querySelectorAll(".shop__mobile-title-item");
+
+for (let i = 0; i < shopMobileTitle.length; i++) {
+    shopMobileTitle[i].addEventListener('click', () => {
+        if (i == 0) {
+            addShopMobileShop(i);
+        } else if (i == 1) {
+            addShopMobileProduct(i);
+        } else if (i == 2) {
+            addShopMobileCategory(i);
+        }
+    });
+}
+
+function addShopMobileShop(i) {
+    shopMobileTitle[i].classList.add("active");
+    shopMobileTitle[1].classList.remove("active");
+    shopMobileTitle[2].classList.remove("active");
+    document.querySelector(".shop__mobile-shop").classList.remove("hide-on-mobile");
+    document.querySelector(".shop__mobile-product").classList.add("hide-on-mobile");
+    document.querySelector(".shop__mobile-category").classList.add("hide-on-mobile");
+    document.querySelector(".header__sort-bar").classList.add("hide-on-mobile") 
+}
+
+function addShopMobileProduct(i) {
+    shopMobileTitle[0].classList.remove("active");
+    shopMobileTitle[i].classList.add("active");
+    shopMobileTitle[2].classList.remove("active");
+    document.querySelector(".shop__mobile-shop").classList.add("hide-on-mobile");
+    document.querySelector(".shop__mobile-product").classList.remove("hide-on-mobile");
+    document.querySelector(".shop__mobile-category").classList.add("hide-on-mobile");
+    document.querySelector(".header__sort-bar").classList.remove("hide-on-mobile");
+}
+
+function addShopMobileCategory(i) {
+    shopMobileTitle[0].classList.remove("active");
+    shopMobileTitle[1].classList.remove("active");
+    shopMobileTitle[i].classList.add("active");
+    document.querySelector(".shop__mobile-shop").classList.add("hide-on-mobile");
+    document.querySelector(".shop__mobile-product").classList.add("hide-on-mobile");
+    document.querySelector(".shop__mobile-category").classList.remove("hide-on-mobile");
+    document.querySelector(".header__sort-bar").classList.add("hide-on-mobile");
+}
+
 function tonglePrice() {
     document.querySelector(".header__sort-link-icon").classList.add("uil-arrow-up");
     document.querySelector(".header__sort-link-icon").classList.toggle("uil-arrow-down")
@@ -10,5 +54,14 @@ function openModalOrderMe() {
 window.addEventListener('click', (e) => {
     if (e.target == document.querySelector(".header__mobile-more-modal")) {
         document.querySelector(".header__mobile-more-modal").classList.remove("open");
+    }
+});
+
+window.addEventListener('scroll', () => {
+    const y = this.pageYOffset;
+    if (y > 80) {
+        document.querySelector(".shop__mobile-header").classList.add("scroll");
+    } else {
+        document.querySelector(".shop__mobile-header").classList.remove("scroll");
     }
 });
