884964d0d2ee0252fdaa6f6d9306b55a2aa2a0d9
DangVanCong2301 <dangvancong2301@gmail.com>
2024-06-08 15:52:59 +0700

    edit some error ui

--- assets/js/product.js
+++ assets/js/product.js
@@ -78,14 +78,17 @@ loadProducts();
 function showNavMenu(event) {
     event.preventDefault();
     document.querySelector(".header__mobile-menu-modal").classList.add("open");
+    document.querySelector(".header__mobile-menu-container").classList.add("open");
 }
 
 function closeNavMenu() {
+    document.querySelector(".header__mobile-menu-container").classList.remove("open");
     document.querySelector(".header__mobile-menu-modal").classList.remove("open");
 }
 
 window.addEventListener('click', (e) => {
     if (e.target == document.querySelector(".header__mobile-menu-modal")) {
         document.querySelector(".header__mobile-menu-modal").classList.remove("open");
+        document.querySelector(".header__mobile-menu-container").classList.remove("open");
     }
 });
