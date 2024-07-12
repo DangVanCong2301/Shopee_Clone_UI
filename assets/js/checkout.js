f141c05a1bf05fe3c5303fc20468319f5562ba90
DangVanCong2301 <dangvancong2301@gmail.com>
2024-07-12 16:39:14 +0700

    code checkout address form

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -23,25 +23,25 @@ let districts = [
     {
         PK_iDistrictID: 1,
         FK_iCityID: 1,
-        sDistricName: "Hoàng Mai",
+        sDistrictName: "Hoàng Mai",
         sCityName: "Hà Nội"
     },
     {
         PK_iDistrictID: 2,
         FK_iCityID: 1,
-        sDistricName: "Thanh Xuân",
+        sDistrictName: "Thanh Xuân",
         sCityName: "Hà Nội"
     },
     {
         PK_iDistrictID: 3,
         FK_iCityID: 1,
-        sDistricName: "Hai Bà Trưng",
+        sDistrictName: "Hai Bà Trưng",
         sCityName: "Hà Nội"
     },
     {
         PK_iDistrictID: 4,
         FK_iCityID: 2,
-        sDistricName: "Hải Hậu",
+        sDistrictName: "Hải Hậu",
         sCityName: "Nam Định"
     }
 ]
@@ -50,17 +50,23 @@ let streets = [
     {
         PK_iStreetID: 1,
         FK_iDistrictID: 1,
-        sStreetName: "Định Công"
+        sStreetName: "Định Công",
+        sDistrictName: "Hoàng Mai",
+        sCityName: "Hà Nội"
     },
     {
         PK_iStreetID: 2,
         FK_iDistrictID: 1,
-        sStreetName: "Trần Nguyên Đán"
+        sStreetName: "Trần Nguyên Đán",
+        sDistrictName: "Hoàng Mai",
+        sCityName: "Hà Nội"
     },
     {
         PK_iStreetID: 3,
         FK_iDistrictID: 3,
-        sStreetName: "Lê Thanh Nghị"
+        sStreetName: "Lê Thanh Nghị",
+        sDistrictName: "Hai Bà Trưng",
+        sCityName: "Hà Nội"
     }
 ]
 
@@ -77,46 +83,62 @@ getAPICheckout();
 function setDataAddressNewChoose() {
     let htmlCities = "";
     for (let i = 0 ; i < cities.length; i++) {
-        htmlCities += " <li class='address-form__add-detail-city-item' onclick='chooseDistrictNew(" + cities[i].PK_iCityID + ")'>";
+        htmlCities += " <li class='address-form__add-detail-city-item' onclick='chooseCityNew(" + cities[i].PK_iCityID + ")'>";
         htmlCities += "" + cities[i].sCityName + "";
         htmlCities += " </li>";
     }
     document.querySelector(".address-form__new-choose-city-list").innerHTML = htmlCities;
 }
 
-function chooseDistrictNew(PK_iCityID) {
+function chooseCityNew(PK_iCityID) {
+    var city = cities.find((obj) => {
+        return obj.PK_iCityID === PK_iCityID;
+    });
+
     let htmlDistricts = "";
     for (let i = 0; i < districts.length; i++) {
         if (districts[i].FK_iCityID == PK_iCityID) {
             addAddressNewChooseDistrictList();
             htmlDistricts +=
                 `
-                    <li class="address-form__add-detail-district-item" onclick="chooseStreetNew(${districts[i].PK_iDistrictID})">
-                        ${districts[i].sDistricName}
+                    <li class="address-form__add-detail-district-item" onclick="chooseDistrictNew(${districts[i].PK_iDistrictID})">
+                        ${districts[i].sDistrictName}
                     </li>
                 `;
         }
     }
     document.querySelector(".address-form__new-choose-district-list").innerHTML = htmlDistricts;
-    document.querySelector(".address-form__new-input").value = sCityName;
+    document.querySelector(".address-form__new-label-choose").style.display = 'none';
+    document.querySelector(".address-form__new-input-choose").value = city.sCityName;
 }
 
-function chooseStreetNew(PK_iDistrictID) {
+function chooseDistrictNew(PK_iDistrictID) {
+    var district = districts.find((obj) => {
+        return obj.PK_iDistrictID === PK_iDistrictID;
+    });
+
     let htmlStreets = "";
     for (let i = 0; i < streets.length; i++) {
         if (streets[i].FK_iDistrictID == PK_iDistrictID) {
-            console.log(streets[i]);
             addAddressNewChooseStreetList();
             htmlStreets += 
             `
-                <li class="address-form__add-detail-street-item">
+                <li class="address-form__add-detail-street-item" onclick="chooseStreetNew(${streets[i].PK_iStreetID})">
                     ${streets[i].sStreetName}
                 </li>
             `;
         }
     }
     document.querySelector(".address-form__new-choose-street-list").innerHTML = htmlStreets;
+    document.querySelector(".address-form__new-input-choose").value = district.sCityName + ", " + district.sDistrictName;
     changeTitleAddressNewChoose();
+}
+
+function chooseStreetNew(PK_iStreetID) {
+    var street = streets.find((obj) => {
+        return obj.PK_iStreetID === PK_iStreetID;
+    });
+    document.querySelector(".address-form__new-input-choose").value = street.sCityName + ", " + street.sDistrictName + ", " + street.sStreetName;
 }
 
 function changeTitleAddressNewChoose() {
@@ -191,7 +213,7 @@ function openNewAddressForm() {
     addAddressForm.style.right = '0';
 }
 
-// Set 1 cookie
+// Set 1 cookie (Nguồn: https://www.tabnine.com/academy/javascript/how-to-set-cookies-javascript/)
 let userID = 1;
 
 function setCookie(userID, value, expDays) {
@@ -215,4 +237,4 @@ function getCookies(userID) {
 console.log(getCookies("userID")); 
 
 // delete cookie
-document.cookie = "userID=;expires=2024-07-11T01:45:13.000Z;path=/"
+//document.cookie = "userID=;expires=2024-07-11T01:45:13.000Z;path=/"
--- assets/js/validate.js
+++ assets/js/validate.js
@@ -11,30 +11,108 @@ function removeErrStyles(label, inputBox) {
     label.classList.add("hide");
 }
 
-function fullNameValidate() {
-    const fullNameInput = document.querySelector(".address-form__new-input");
-    const fullNameLabel = document.querySelector(".address-form__add-label-new");
-    let fullName = fullNameInput.value;
+let isValidate = true;
+function fullNameNewAddressValidate() {
+    const fullNameNewAddressInput = document.querySelector(".address-form__new-input-fullname");
+    const fullNameNewAddressLabel = document.querySelector(".address-form__new-label-fullname");
+    let fullName = fullNameNewAddressInput.value;
 
     // Xoá khoảng cách
     fullName = fullName.replaceAll(" ", "");
     if (fullName === "") {
-        showErrStyles(fullNameLabel, fullNameInput);
+        showErrStyles(fullNameNewAddressLabel, fullNameNewAddressInput);
+        isValidate = false;
     } else {
-        removeErrStyles(fullNameLabel, fullNameInput);
+        removeErrStyles(fullNameNewAddressLabel, fullNameNewAddressInput);
+        isValidate = true;
     }
+    return isValidate;
+}
+
+function phoneNewAddressValidate() {
+    const phoneNewAddressInput = document.querySelector(".address-form__new-input-phone");
+    const phoneNewAddressLabel = document.querySelector(".address-form__new-label-phone");
+
+    let phone = phoneNewAddressInput.value;
+    phone = phone.replaceAll(" ", "");
+
+    if (phone === "") {
+        showErrStyles(phoneNewAddressLabel, phoneNewAddressInput);
+        isValidate = false;
+    } else {
+        removeErrStyles(phoneNewAddressLabel, phoneNewAddressInput);
+        isValidate = true;
+    }
+    return isValidate;
+}
+
+function chooseNewAddressValidate() {
+    const chooseNewAddressInput = document.querySelector(".address-form__new-input-choose");
+    const chooseNewAddressLabel = document.querySelector(".address-form__new-label-choose");
+
+    let choose = chooseNewAddressInput.value;
+    choose = choose.replaceAll(" ", "");
+
+    if (choose === "") {
+        showErrStyles(chooseNewAddressLabel, chooseNewAddressInput);
+        isValidate = false;
+    } else {
+        removeErrStyles(chooseNewAddressLabel, chooseNewAddressInput);
+        isValidate = true;
+    }
+    return isValidate;
+}
+
+function descNewAddressValidate() {
+    const descNewAddressInput = document.querySelector(".address-form__new-textarea-desc");
+    const descNewAddressLabel = document.querySelector(".address-form__new-label-desc");
+
+    let desc = descNewAddressInput.value;
+    desc = desc.replaceAll(" ", "");
+
+    if (desc === "") {
+        showErrStyles(descNewAddressLabel, descNewAddressInput);
+        isValidate = false;
+    } else {
+        removeErrStyles(descNewAddressLabel, descNewAddressInput);
+        isValidate = true;
+    }
+    return isValidate;
 }
 
 function addEvent() {
-    const fullNameInput = document.querySelector(".address-form__new-input");
-    const addressNewBtn = document.querySelector(".address-form__add-new");
+    const fullNameNewAddressInput = document.querySelector(".address-form__new-input-fullname");
+    const phoneNewAddressInput = document.querySelector(".address-form__new-input-phone");
+    const chooseNewAddressInput = document.querySelector(".address-form__new-input-choose");
+    const descNewAddressInput = document.querySelector(".address-form__new-textarea-desc");
+    const addressNewAddressBtn = document.querySelector(".address-form__new-btn");
 
-    fullNameInput.addEventListener('blur', () => {
-        fullNameValidate();
+    fullNameNewAddressInput.addEventListener('blur', () => {
+        fullNameNewAddressValidate();
     });
 
-    addressNewBtn.addEventListener("click", () => {
-        fullNameValidate();
-    })
+    phoneNewAddressInput.addEventListener('blur', () => {
+        phoneNewAddressValidate();
+    });
+
+    chooseNewAddressInput.addEventListener('blur', () => {
+        chooseNewAddressValidate();
+    });
+
+    descNewAddressInput.addEventListener('blur', () => {
+        descNewAddressValidate();
+    });
+
+    addressNewAddressBtn.addEventListener("click", () => {
+        fullNameNewAddressValidate();
+        phoneNewAddressValidate();
+        chooseNewAddressValidate();
+        descNewAddressValidate();
+        if (fullNameNewAddressValidate() && phoneNewAddressValidate() && chooseNewAddressValidate() && descNewAddressValidate()) {
+            setCookie("userID", 1, 30);
+            closeAddressModal();
+            toast({title: "Thông báo", msg: `Thêm địa chỉ thành công`, type: "success", duration: 5000}); 
+        };
+    });
 }
 addEvent();
