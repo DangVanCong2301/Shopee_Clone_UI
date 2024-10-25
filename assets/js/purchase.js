aa0849a9a5656d119a821293158ebcffc698f631
dangvancong2301@gmail.com <dangvancong2301@gmail.com>
2024-10-25 22:08:24 +0700

    reviewer form

--- assets/js/purchase.js
+++ assets/js/purchase.js
@@ -41,6 +41,63 @@ for (let i = 0; i < headerItem.length; i++) {
             document.querySelector(".purchase__all").classList.add("hide-on-mobile");
             document.querySelector(".purchase__wait").classList.add("hide-on-mobile");
             document.querySelector(".purchase__transport").classList.remove("hide-on-mobile");
+        } else if (i == 3) {
+            // Chờ giao hàng
+            headerItem[i].classList.add("active");
+            headerItem[0].classList.remove("active");
+            headerItem[1].classList.remove("active");
+            headerItem[2].classList.remove("active");
+            document.querySelector(".purchase__all").classList.add("hide-on-destop");
+            document.querySelector(".purchase__wait").classList.add("hide-on-destop");
+            document.querySelector(".purchase__transport").classList.add("hide-on-destop");
+            document.querySelector(".purchase__wait-delivery").classList.remove("hide-on-destop");
+        } else if (i == 4) {
+            // Hoàn thành 
+            headerItem[i].classList.add("active");
+            headerItem[0].classList.remove("active");
+            headerItem[1].classList.remove("active");
+            headerItem[2].classList.remove("active");
+            headerItem[3].classList.remove("active");
+            document.querySelector(".purchase__all").classList.add("hide-on-destop");
+            document.querySelector(".purchase__wait").classList.add("hide-on-destop");
+            document.querySelector(".purchase__transport").classList.add("hide-on-destop");
+            document.querySelector(".purchase__wait-delivery").classList.add("hide-on-destop");
+            document.querySelector(".purchase__complete").classList.remove("hide-on-destop");
         }
     });
+}
+
+// Upload Image
+function uploadImage() {
+    let profilePic = document.querySelector(".reviewer-form__thumb-img-value");
+    let inputFile = document.getElementById("input-file");
+    console.log(inputFile.files[0].name);
+    profilePic.src = URL.createObjectURL(inputFile.files[0]);
+    profilePic.classList.add("show");
+    document.querySelector(".reviewer-form__thumb-img-add").classList.add("hide");
+}
+
+// Rate
+const rate = document.getElementsByName("rate");
+let rateCheck = "";
+for (let i = 0; i < rate.length; i++) {
+    if (rate.item(i).checked) {
+        rateCheck = rate.item(i).value;
+    }
+    rate.item(i).onchange = () => {
+        if (rate.item(i).value == 5) {
+            document.querySelector(".reviewer-form__quality-text").classList.remove("hide-on-destop");
+        } else {
+            document.querySelector(".reviewer-form__quality-text").classList.add("hide-on-destop");
+        }
+    }
+}
+
+// Modal
+function openModal() {
+    document.querySelector(".modal").classList.add("open");
+}
+
+function closeModal() {
+    document.querySelector(".modal").classList.remove("open");
 }
