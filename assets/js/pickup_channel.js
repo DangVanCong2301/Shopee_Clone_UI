ffc7526bc98f017b431c7710e96482d4e0f9fc0b
dangvancong2301@gmail.com <dangvancong2301@gmail.com>
2025-02-12 17:37:34 +0700

    edit ui picker

--- assets/js/pickup_channel.js
+++ assets/js/pickup_channel.js
@@ -8,3 +8,26 @@ function closeNavMenu() {
     document.querySelector(".header__menu-overlay").classList.remove("open");
     document.querySelector(".header__menu-container").classList.remove("open");
 }
+
+// Modal
+function openModalMobile() {
+    document.querySelector(".modal").classList.add("open");
+}
+
+function closeModalMobile() {
+    document.querySelector(".modal").classList.remove("open");
+}
+
+function openReceiveOrderModal(shippingOrderID) {
+    openModalMobile();
+    document.querySelector(".modal__body").innerHTML = 
+    `
+                            <div class="modal__confirm-mobile">
+                                <div class="modal__confirm-mobile-msg">Bạn có chắc muốn nhận đơn hàng này?</div>
+                                <div class="modal__confirm-mobile-btns">
+                                    <div class="modal__confirm-mobile-btn-no" onclick="closeModal()">Không</div>
+                                    <div class="modal__confirm-mobile-btn-agree" onclick="confirmTakeOrder(${shippingOrderID})">Đồng ý</div>
+                                </div>
+                            </div>
+    `;
+}
